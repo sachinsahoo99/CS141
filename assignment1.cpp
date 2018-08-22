21ecc92ef0c0213a86617a4a9862eaf316330f65
sachinsahoo99 <42239222+sachinsahoo99@users.noreply.github.com>
2018-08-22 18:26:18 +0530

    Add files via upload

---
+++ assignment1.cpp
@@ -0,0 +1,25 @@
+//including library
+#include <iostream>
+using namespace std;
+int main()
+{
+
+//assigning values
+
+float length,meter,kilometer;
+
+cout <<"enter the length in centimeter";
+cin >> length;
+cout << "the value entered ="<<length<<endl;
+
+//conversion factors
+
+meter=length/100;
+kilometer=length/100000;
+
+cout <<"meter = ";
+cout <<meter<<endl;
+cout <<"kilometer = ";
+cout <<kilometer<<endl;
+return 6612;
+}
---
+++ assignment2.cpp
@@ -0,0 +1,24 @@
+//including library
+#include <iostream>
+using namespace std;
+int main()
+{
+
+//assigning the values
+
+
+float temperature,fahrenheit,(celsius);
+
+cout <<"enter the temperature in celsius";
+cin >>temperature;
+cout <<"the value entered ="<<temperature<<endl;
+
+//conversion factors
+
+fahrenheit=9/5*(celsius)+32;
+
+cout <<"fahrenheit = ";
+cout <<temperature<<endl;
+
+//LET IT RAIN
+}
