0465f223c4036513d1305a8b9e385f3fcae6b897
sachinsahoo99 <42239222+sachinsahoo99@users.noreply.github.com>
2018-08-22 19:23:12 +0530

    Add files via upload

---
+++ assignment3.cpp
@@ -0,0 +1,25 @@
+//including library
+#include <iostream>
+using namespace std;
+int main()
+{
+
+//assigning the values
+
+
+float fahrenheit,celsius;
+
+cout <<"enter the temperature in fahrenheit";
+cin >>fahrenheit;
+cout <<"the value entered ="<<fahrenheit<<endl;
+
+//conversion factors
+
+celsius=(5*fahrenheit-160)/9;
+
+cout <<"celsius = ";
+cout <<celsius<<endl;
+return 9;
+
+//LET IT SNOW
+}
